f65484d041a38f97f970ae8c603b7d6d31a84d8c
DimaGenkai <92301623+w1zzzle@users.noreply.github.com>
2023-07-31 16:12:02 +0400

    #2 hp

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
+import React from 'react';
+import './scss/app.scss';
+import Header from './components/Header';
+import Categories from './components/Categories';
+import Sort from './components/Sort';
+import PizzaBlock from './components/PizzaBlock';
+
 function App() {
   return (
-    <div className="App">
-      <h1>123</h1>
-    </div>
+        <div class="wrapper">
+          <Header />
+          <div class="content">
+            <div class="container">
+              <div class="content__top">
+                <Categories />
+                <Sort />
+              </div>
+              <h2 class="content__title">Все пиццы</h2>
+              <div class="content__items">
+                <PizzaBlock title='Mexico-pizza' price={500}/>
+                <PizzaBlock />
+                <PizzaBlock />
+                <PizzaBlock />
+                <PizzaBlock />
+                <PizzaBlock />
+                <PizzaBlock />
+                <PizzaBlock />
+                <PizzaBlock />
+              </div>
+            </div>
+          </div>
+        </div>
   );
 }
 
---
+++ src/components/Categories.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Categories() {
+  return (
+    <div>
+      <div class="categories">
+        <ul>
+          <li class="active">Все</li>
+          <li>Мясные</li>
+          <li>Вегетарианская</li>
+          <li>Гриль</li>
+          <li>Острые</li>
+          <li>Закрытые</li>
+        </ul>
+      </div>
+    </div>
+  );
+}
+
+export default Categories;
